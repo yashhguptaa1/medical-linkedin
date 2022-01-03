f5edc56d1d65b52f608b435a64c4cded8b35b629
Yash Gupta <yashhguptaa1@gmail.com>
2022-01-03 19:14:12 +0530

    spring project added

---
+++ linkedin/src/main/java/com/yg/linkedin/LinkedinApplication.java
@@ -0,0 +1,13 @@
+package com.yg.linkedin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedinApplication.class, args);
+	}
+
+}
---
+++ linkedin/src/test/java/com/yg/linkedin/LinkedinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yg.linkedin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
